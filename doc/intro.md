09147ca2d24d434f5b2a609280b7c049a27c87d8
Lei Wang <tendant@gmail.com>
2020-11-28 20:10:12 -0800

    add jws cookies backend

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to jwt-backend
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/jwt_backend/jws.clj
@@ -0,0 +1,42 @@
+(ns jwt-backend.jws
+  (:require [buddy.auth.protocols :as proto]
+            ;; [buddy.auth.http :as http]
+            [buddy.auth :as auth]
+            [buddy.sign.jwt :as jwt]))
+
+(defn- handle-unauthorized-default
+  "A default response constructor for an unauthorized request."
+  [request]
+  (if (auth/authenticated? request)
+    {:status 403 :headers {} :body "Permission denied"}
+    {:status 401 :headers {} :body "Unauthorized"}))
+
+(defn- parse-cookies
+  [request token-name]
+  (some->> (get-in request [:cookies "Authorization" :value])
+           (re-find (re-pattern (str "^" token-name " (.+)$")))
+           (second)))
+
+(defn jws-backend
+  [{:keys [secret authfn unauthorized-handler options token-name on-error]
+    :or {authfn identity token-name "Token"}}]
+  {:pre [(ifn? authfn)]}
+  (reify
+    proto/IAuthentication
+    (-parse [_ request]
+      (parse-cookies request token-name))
+
+    (-authenticate [_ request data]
+      (try
+        (authfn (jwt/unsign data secret options))
+        (catch clojure.lang.ExceptionInfo e
+          (let [data (ex-data e)]
+            (when (fn? on-error)
+              (on-error request e))
+            nil))))
+
+    proto/IAuthorization
+    (-handle-unauthorized [_ request metadata]
+      (if unauthorized-handler
+        (unauthorized-handler request metadata)
+        (handle-unauthorized-default request)))))
