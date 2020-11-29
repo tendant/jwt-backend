702eb2ac898e4b613a7eae02a9cc586ba9715211
Lei Wang <tendant@gmail.com>
2020-11-28 20:15:53 -0800

    release 0.1.1

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject tendant/jwt-backend "0.1.0"
+(defproject tendant/jwt-backend "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [buddy/buddy-sign "3.2.0"]
+                 [buddy/buddy-auth "2.2.0"]]
   :repl-options {:init-ns jwt-backend.core})
